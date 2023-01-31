7cf3b2af86b9fb9f1f6b43e465f3f059ab16c98a
강은혜 <91363321+kkangeee@users.noreply.github.com>
2023-01-31 22:39:44 +0900

    [Bronze V] Title: ??!, Time: 220 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		a = a.toLowerCase();
+		System.out.println(a + "??!");
+    }
+}
