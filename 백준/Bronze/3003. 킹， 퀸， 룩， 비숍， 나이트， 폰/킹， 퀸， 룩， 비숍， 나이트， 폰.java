5d560c5a47f66041b0c21200c11eefddc28179c1
강은혜 <91363321+kkangeee@users.noreply.github.com>
2023-01-31 22:40:25 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 340 ms, Memory: 19260 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		int E = sc.nextInt();
+		int F = sc.nextInt();
+		
+		System.out.println((1-A) + " " + (1-B) + " " + (2-C) + " " + (2-D) + " " + (2-E) + " " + (8-F));
+		
+	}
+
+}
