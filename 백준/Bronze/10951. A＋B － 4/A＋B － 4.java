8dc49a08ad332338369c42efe9364f5a2a5ea021
강은혜 <91363321+kkangeee@users.noreply.github.com>
2023-02-10 00:03:21 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+			
+		while(true) {
+			try {
+                st = new StringTokenizer(br.readLine(), " ");
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				System.out.println(a+b);
+			}catch (NullPointerException e) {
+				break;
+			}
+			
+		}
+		
+	}
+}
