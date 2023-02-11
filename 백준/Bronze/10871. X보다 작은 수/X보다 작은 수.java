66673719a13ef28f028d760cb83da3f1c6f7085c
강은혜 <91363321+kkangeee@users.noreply.github.com>
2023-02-11 20:46:26 +0900

    Update X보다 작은 수.java

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -2,7 +2,26 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//1번
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < a; i++) {
+			int value = Integer.parseInt(st.nextToken());
+			if (value < b)
+				sb.append(value).append(' ');
+		}
+		System.out.println(sb);
+	}
+}
+
+//2번
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner scanner = new Scanner(System.in);
